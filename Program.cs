64fb7f6f2b7113d8bb297e06c5d07591e2e05ecb
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-02-28 01:35:25 +0300

    ijunior_csharp_dz28

--- Program.cs
+++ Program.cs
@@ -7,168 +7,135 @@ namespace vn_mode_csharp_dz28
         static void Main(string[] args)
         {
             bool isOpen = true;
-            string[] fullNames = { "Бабин Василий Петрович - СТРОИТЕЛЬ", "Нагибин Виталий Павлович - МУЗЫКАНТ", "Марук Юрий Семёнович - ВОДИТЕЛЬ"};
-            string[] positions = {"СТРОИТЕЛЬ", "МУЗЫКАНТ", "ВОДИТЕЛЬ"};
-            string userFirstName, userLastName, userPatronymic, userPositionAtWork;
-            bool isSearch = false;
-            int counter = 0;
+            string[] dossier = new string[0];
+            string[] position = new string[0];
 
             while (isOpen)
             {
-                Console.WriteLine("Доступные команды:\n1 - Добавить досье\n2 - Вывести все досье\n3 - Удалить досье\n4 - Поиск по фамилии\n5 - Выход");
-                Console.Write("\nВведите команду :");
+                Console.WriteLine("Доступные команды:\n");
+                Console.WriteLine("1 - Добавить досье");
+                Console.WriteLine("2 - Показать все досье");
+                Console.WriteLine("3 - Удалить выбранное досье");
+                Console.WriteLine("4 - Поиск досье по фамилии");
+                Console.WriteLine("5 - Выход из программы");
+
+                Console.Write("Введите номер команды: ");
+
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        showMessage("Добавить досье:");
-                        Console.Write("Введите фамилию: ");
-                        userLastName = Console.ReadLine();
-                        Console.Write("Введите имя: ");
-                        userFirstName = Console.ReadLine();
-                        Console.Write("Введите очество: ");
-                        userPatronymic = Console.ReadLine();
-                        Console.Write("Введите должность: ");
-                        userPositionAtWork = Console.ReadLine();
-                        AddDossier(ref fullNames, ref positions, userFirstName, userLastName, userPatronymic, userPositionAtWork);
-                        showMessage("Вы успешно добавили новое досье.");
+                        AddDossier(ref dossier, ref position);
                         break;
                     case "2":
-                        showMessage("Все досье:");
-                        showAllDossiers(fullNames, positions);
+                        ShowDossier(dossier, position);
                         break;
                     case "3":
-                        showMessage("Вы выбрали команду 'Удалить досье'.");
-                        Console.Write("Введите желаемую фамилию: ");
-                        userLastName = Console.ReadLine();
-                        DeleteDossier(ref fullNames, ref positions, userLastName, out isSearch);
-                        if (!isSearch)
-                        {
-                            isSearch = false;
-                            showMessage("Ничего не найдено", ConsoleColor.DarkRed);
-                        }
+                        DeleteDossier(ref dossier, ref position);
                         break;
                     case "4":
-                        showMessage("Вы выбрали команду 'Поиск по фамилии'.");
-                        Console.Write("Введите фамилию: ");
-                        userLastName = Console.ReadLine();
-                        SearchLastName(fullNames, userLastName, ref counter, out isSearch);
-                        if (!isSearch)
-                        {
-                            isSearch = false;
-                            showMessage("Ничего не найдено", ConsoleColor.DarkRed);
-                        }
+                        FindDossier(dossier, position);
                         break;
                     case "5":
-                        showMessage("Вы вышли из программы.");
                         isOpen = false;
-                        Console.ReadKey();
-                        break;
-                    default:
-                        showMessage("Вы ввели не существующую команду.", ConsoleColor.DarkRed);
                         break;
                 }
+
                 Console.ReadKey();
                 Console.Clear();
             }
-
-            
+            Console.WriteLine("Вы вышли из программы.");
         }
 
-        static void AddDossier(ref string[] fullNames, ref string[] positions, string userFirstName, string userLastName, string userPatronymic, string userPositionAtWork)
+        static string[] ExpansionArray(ref string[] array)
         {
-            string[] tempFullNames = new string[fullNames.Length + 1];
+            string[] tempArray = new string[array.Length + 1];
 
-            for (int i = 0; i < fullNames.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                tempFullNames[i] = fullNames[i];
+                tempArray[i] = array[i];
             }
 
-            tempFullNames[tempFullNames.Length - 1] = userLastName + " " + userFirstName + " " + userPatronymic + " - ";
-            fullNames = tempFullNames;
-
-            string[] tempPositions = new string[positions.Length + 1];
-
-            for (int i = 0; i < positions.Length; i++)
-            {
-                tempPositions[i] = positions[i];
-            }
-
-            tempPositions[tempPositions.Length - 1] = userPositionAtWork;
-            positions = tempPositions;
+            array = tempArray;
+            return array;
         }
 
-        static void SearchLastName(string[] fullNames, string lastName, ref int index, out bool isSearch)
+        static string[] CompressionArray(ref string[] array, int indexForCompressed)
         {
-            isSearch = false;
-            index = 0;
-            foreach (string dossier in fullNames)
+            string[] tempArray = new string[array.Length - 1];
+
+            for (int i = 0; i < indexForCompressed - 1; i++)
             {
-                string[] tempFullNames = dossier.Split(' ');
-                if (tempFullNames[0].ToLower() == lastName.ToLower())
+                tempArray[i] = array[i];
+            }
+
+            for (int i = indexForCompressed; i < array.Length; i++)
+            {
+                tempArray[i - 1] = array[i];
+            }
+
+            array = tempArray;
+            return array;
+        }
+
+        static void AddDossier(ref string[] dossier, ref string[] position)
+        {
+            Console.WriteLine("Заполните ФИО сотрудника:");
+            ExpansionArray(ref dossier);
+            dossier[dossier.Length - 1] = Console.ReadLine();
+            Console.WriteLine("На какой должности сотрудник работает?");
+            ExpansionArray(ref position);
+            position[position.Length - 1] = Console.ReadLine();
+            showMessage("Досье успешно добавлено.");
+        }
+
+        static void ShowDossier(string[] dossier, string[] position)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Список всех досье:");
+
+            for (int i = 0; i < dossier.Length; i++)
+            {
+                if (dossier[i] != "" && position[i] != "")
                 {
-                    index = index + 1;
-                    Console.WriteLine(index + ") " + dossier);
-                    isSearch = true;
+                    Console.WriteLine((i + 1) + ". " + dossier[i] + " - " + position[i]);
+                }
+            }
+            if (dossier.Length == 0)
+            {
+                showMessage("Отсутствуют досье", ConsoleColor.DarkRed);
+            }
+        }
+
+        static void DeleteDossier(ref string[] dossier, ref string[] position)
+        {
+            Console.WriteLine("Досье под каким номером нужно удалить?");
+            int indexForDeleating = Convert.ToInt32(Console.ReadLine());
+            CompressionArray(ref dossier, indexForDeleating);
+            CompressionArray(ref position, indexForDeleating);
+            showMessage("Досье успешно удалено.");
+        }
+
+        static void FindDossier(string[] dossier, string[] position)
+        {
+            Console.WriteLine("Введите фамилию для поиска:");
+            string userInput = Console.ReadLine();
+
+            for (int i = 0; i < dossier.Length; i++)
+            {
+                string fullName = dossier[i];
+                string[] surnameFind = fullName.Split(' ');
+
+                if (userInput.ToLower() == surnameFind[0].ToLower())
+                {
+                    Console.WriteLine((i + 1) + ". " + dossier[i] + " - " + position[i]);
                 }
             }
         }
-
-        static void DeleteDossier(ref string[] fullNames,ref string[] position, string lastName, out bool isSearch)
-        {
-            isSearch = false;
-            foreach (var fullName in fullNames)
-            {
-                string[] tempFullNames = fullName.Split(' ');
-                if (tempFullNames[0].ToLower() == lastName.ToLower())
-                {
-                    Console.WriteLine(fullName);
-                    isSearch = true;
-                    string[] arrayFullNameForDelete = new string[1];
-                    arrayFullNameForDelete[0] = fullName;
-                    string[] arrayPositionForDelete = new string[1];
-                    arrayPositionForDelete = position;
-                    showMessage("Вы хотите удалить это досье? (y/n): ", ConsoleColor.DarkYellow);
-                    if (Console.ReadLine().ToLower() == "y")
-                    {
-                        string[] tempArrayForDelete = new string[fullNames.Length];
-                        string[] tempArrayPositionForDelete = new string[position.Length];
-                        for (int i = 0; i < tempArrayForDelete.Length; i++)
-                        {
-                            if (fullNames[i].ToLower() != arrayFullNameForDelete[0].ToLower())
-                            {
-                                tempArrayForDelete[i] = fullNames[i];
-                                tempArrayPositionForDelete[i] = position[i];
-                            }
-                        }
-
-                        fullNames = tempArrayForDelete;
-                        position = tempArrayPositionForDelete;
-                        showMessage("Вы успешно удалили досье", ConsoleColor.DarkGreen);
-                    }
-                }
-            }
-
-        }
-
         static void showMessage(string message, ConsoleColor color = ConsoleColor.DarkGreen)
         {
             Console.ForegroundColor = color;
             Console.WriteLine(message);
             Console.ForegroundColor = ConsoleColor.White;
-        }
-
-        static void showAllDossiers(string[] fullNames, string[] positions)
-        {
-            int indexDossier = 0;
-            for (int i = 0; i < fullNames.Length; i++)
-            {
-                if (fullNames[i] != null)
-                {
-                    indexDossier++;
-                    Console.WriteLine(indexDossier + ") " + fullNames[i] + positions[i]);
-                }
-            }
-
         }
     }
 }
